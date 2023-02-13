fd0c2627fde36beeca4397ebb8735aa436bc015a
Genokrad <104651129+Genokrad@users.noreply.github.com>
2023-02-13 11:38:50 +0000

    Deploying to gh-pages from @ Genokrad/goit-react-hw-08-phonebook@129e1c226b7176e42fbcaaf0737792e5bf655cc2 🚀

---
+++ static/js/415.79ce69cc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,o){o.r(t);var r=o(184);t.default=function(){return(0,r.jsx)("h1",{children:"Welcome to Phonebook App"})}}}]);
+//# sourceMappingURL=415.79ce69cc.chunk.js.map
